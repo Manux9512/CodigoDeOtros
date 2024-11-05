f9419b773cba077774350512468e9d8467625edd
Manuel <jmanuel.lmz@gmail.com>
2024-11-05 17:02:05 -0600

    Second commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+//Se modifico el elemento dentro del querySelector a .name y .blog
+//La funcion displayUser es asyncrona
+//Se definio la constante data 
+//Se cambiaron los '' por backticks `` en las variables n, b y l
+
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
+const $l = document.querySelector('.location');
+
+async function displayUser(username) {
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();
+  console.log(data);
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
+}
+
+function handleError(err) {
+  console.log('OH NO!');
+  console.log(err);
+  n.textContent = `Algo salió mal: ${err}`;
+}
+
+displayUser('stolinski').catch(handleError);
